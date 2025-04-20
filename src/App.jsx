82a42569659d8e45cdab8eb869ac226c6083c57d
Jeremy Gamer <jeremy.leyvraz@gmail.com>
2025-04-20 17:12:34 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
+      <h1 className="text-white text-4xl font-bold">Bienvenue sur mon CV en ligne 🚀</h1>
+    </div>
+  )
+}
+
+export default App
