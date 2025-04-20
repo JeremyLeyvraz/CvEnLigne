6729ca9e91d3f844182410a2ac445012c4f781c9
Jeremy Gamer <jeremy.leyvraz@gmail.com>
2025-04-20 18:05:31 +0200

    test maj + deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
-      <h1 className="text-white text-4xl font-bold">Bienvenue sur mon CV en ligne 🚀</h1>
+      <h1 className="text-white text-4xl font-bold">Bienvenue sur mon CV en ligne ! 🚀</h1>
     </div>
   )
 }
